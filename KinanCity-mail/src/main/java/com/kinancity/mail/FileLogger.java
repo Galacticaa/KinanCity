e9777772cbed7be759b83e67cd0181582ce2d271
Galactica <Galacticaa@users.noreply.github.com>
2017-07-15 06:54:28 +0100

    Update database on successful link activation
    This could probably be done in a much better place, but good enough.

--- KinanCity-mail/src/main/java/com/kinancity/mail/FileLogger.java
+++ KinanCity-mail/src/main/java/com/kinancity/mail/FileLogger.java
@@ -1,5 +1,13 @@
 package com.kinancity.mail;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,13 +18,58 @@ public class FileLogger {
 	public static final String BAD = "BAD";
 	public static final String THROTTLED = "THROTTLED";
 	public static final String ERROR = "ERROR";
-	
+
 	public static final String SKIPPED = "SKIPPED";
 
 	private static Logger LOGGER = LoggerFactory.getLogger("LINKS");
+	private static Logger errlog = LoggerFactory.getLogger("com.kinancity.mail");
+
+	private static Connection dbConn = null;
 
 	public static void logStatus(Activation link, String status) {
 		LOGGER.info("{};{};{}", link.getLink(), link.getEmail(), status);
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			String dsn = String.format("jdbc:mysql://%s:%s/%s", "localhost", 3306, "somedb");
+			dbConn = DriverManager.getConnection(dsn, "root", "root");
+
+			if (dbConn == null) {
+				errlog.info("Failed to connect to database.");
+				return;
+			}
+
+			PreparedStatement query;
+
+			switch (status) {
+				case OK:
+					query = dbConn.prepareStatement("UPDATE accounts SET activated_at=? WHERE email=?");
+					query.setTimestamp(1, Timestamp.from(Instant.now()));
+					query.setString(2, link.getEmail());
+					query.executeUpdate();
+					query.close();
+					break;
+				case DONE:
+					query = dbConn.prepareStatement("SELECT activated_at FROM accounts WHERE email=? LIMIT 1");
+
+					ResultSet result = query.executeQuery();
+					if (result.next() && result.getTimestamp("activated_at") == null) {
+						result.updateTimestamp("activated_at", Timestamp.from(Instant.now()));
+						result.updateRow();
+					}
+					query.close();
+					break;
+				default:
+					break;
+			}
+
+			dbConn.close();
+		} catch (ClassNotFoundException e) {
+			errlog.info("Could not find JDBC driver.");
+		} catch (SQLException e) {
+			errlog.info("Failed to set activation date for " + link.getEmail());
+		}
 	}
 
 	public static Activation fromLog(String line) {
